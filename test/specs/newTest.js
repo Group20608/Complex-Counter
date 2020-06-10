1e51d76a8555cd0b2ba927d5a098067f37dd4fbd
cyxoi1 <cyxoi1@yandex.ru>
2020-06-10 21:57:12 +0100

    page header check test added

--- test/specs/newTest.js
+++ test/specs/newTest.js
@@ -8,6 +8,16 @@ describe('page', () => {
     it('should have the right title', () => {
         browser.url('https://likejean.github.io/homework-5/')
         expect(browser.getTitle()).eq('Complex Counter App');
+        browser.pause(2000);
     })
+
+
+    it('the page header should be Counters', () => {
+
+        expect($('//h1[@class=\'header\']').getText()).equal('Counters');
+        browser.pause(2000);
+    });
+
+
 })
 // + test
