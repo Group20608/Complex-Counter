fcfb690507386a5ffd67b00dd1dafa700a7b8977
Anastasiia Paromova <miss.paromova@gmail.com>
2020-06-23 09:57:16 -0700

    updated version

--- test/specs/CCA/pageObject/Data.js
+++ test/specs/CCA/pageObject/Data.js
@@ -1,13 +1,16 @@
-const Header = {
+const DataPage = {
     title: 'Counters',
     total: 'Total: 0',
     LF1: 'CHANGE STEP OPTIONS?',
     LF2: 'CHANGE STEP OPTIONS?',
     defaultCounter: '1. Default Counter',
     editCounterTitle: 'Edit Counter Title:',
-    defaultCounterText: 'Default Counter'
+    defaultCounterText: 'Default Counter',
+    appTitle: 'Complex Counter App',
+    enterCounterTitle: 'Enter Counter Title:',
+
 
 
 
 }
-export {Header}
+export {DataPage}
