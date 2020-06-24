19d2734a606b54fb1cedb5d13bef2341868ce524
Stanislav Maslov <maslov1500@gmail.com>
2020-06-23 23:19:07 -0700

    PO model tests

--- test/specs/CCA/pageObject/Page.js
+++ test/specs/CCA/pageObject/Page.js
@@ -16,7 +16,7 @@ class Page extends AppPage{
         return $('//button[@name="negative"]');
     }
     get LF2(){
-        return $('button[name="positive"]');
+        return $('//button[@name="positive"]');
     }
     get DeleteBtn(){
         return $('[id="1"]');
@@ -24,7 +24,7 @@ class Page extends AppPage{
     get RestBtn(){
         return $('[class="btn-primary btn Ripple-parent reset"]');
     }
-    get defaultCounter(){
+    get defaultCounterFirst(){
         return $$('h3')[1];
     }
     get editCounterTitle(){
@@ -36,6 +36,28 @@ class Page extends AppPage{
     get enterCounterTitle() {
         return $('//label[contains(text(),"Enter")]');
     }
+    get counterName(){
+        return $('[name="name"]');
+    }
+    get counterValueInput(){
+        return $('[name="value"]');
+    }
+    get addCounterBnt(){
+        return $('[class="btn-success btn Ripple-parent add col-6"]');
+    }
+    get defaultCounterSecond(){
+        return $$('h3')[2];
+    }
+    get limitInputLower(){
+        return $('[name="lower"]')
+    }
+    get limitInputUpper(){
+        return $('input[name="upper"]')
+    }
+    get errorGreaterZero(){
+        return $('span[style="color: red;"]');
+    }
+
 
 }
 export default new Page();
